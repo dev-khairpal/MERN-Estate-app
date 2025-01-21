2739ce3084d82916ba6bff09bb9e794f9a9f8d55
dev-khairpal <133212101+dev-khairpal@users.noreply.github.com>
2025-01-21 17:41:32 +0530

    added UI for signin page

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,9 +1,91 @@
-
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Signin = () => {
-  return (
-    <div>Signin</div>
-  )
-}
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [isLoading, setLoading] = useState(false);
 
-export default Signin
+  const navigate = useNavigate();
+
+  // form data handler
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+// submit handler
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+
+      // if not success in response then error set - response but some error
+      if (data.success === false) {
+        setError(data.message);
+        setLoading(false);
+        return;
+      }
+      setLoading(false);
+      setError(null)
+      navigate('/')
+
+      // no response error catch
+    } catch (err) {
+      setLoading(false);
+      setError(err.message)
+    }
+  };
+
+
+  return (
+    <div className="mx-auto p-4 max-w-lg">
+      <h1 className="text-3xl text-center font-semibold my-8">Sign In</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <input
+          id="email"
+          type="email"
+          className="border p-2 rounded-md"
+          placeholder="email"
+          onChange={handleChange}
+        />
+        <input
+          id="password"
+          type="password"
+          className="border p-2 rounded-md"
+          placeholder="password"
+          onChange={handleChange}
+        />
+        <button
+          disabled={isLoading}
+          type="submit"
+          className="bg-blue-500 text-white p-2 rounded-md hover:bg-blue-400 transition-colors"
+        >
+          Sign In
+        </button>
+      </form>
+      <div className="text-center mt-2">
+        <p>
+          Dont have an Account ?{" "}
+          <Link className="text-red-400 hover:underline" to="/sign-up">
+            Sign Up
+          </Link>
+        </p>
+      </div>
+      {error && <p className="text-red-600">{error}</p>}
+    </div>
+  );
+};
+
+export default Signin;
